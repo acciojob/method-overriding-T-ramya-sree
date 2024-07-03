6d6488703a4583f8e12b83e45de4e950d89d3a45
Ramya Shree <kramyasree505@gmail.com>
2024-07-03 22:43:43 +0530

    S3

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,23 @@
 package com.driver.test;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+
+import com.driver.A;
+import com.driver.B;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+
+    @Test
+    public void testMethodINA() {
+        A a = new A();
+        Assertions.assertEquals("Invoking method from class A", a.meth());
+    }
+
+    @Test
+    public void testMethodINB() {
+        B b = new B();
+        Assertions.assertEquals("Method is overridden in Extendend class B", b.meth());
+    }
 }
